39fb8b0e181433140bcbd31ed4e805d17ae29c83
nir <nirshahar@mail.tau.ac.il>
2021-10-03 17:42:49 +0300

    Change `arg_max` and `arg_min` API - Now returns a `usize` instead of `Option<usize>`

--- src/miscellaneous.rs
+++ src/miscellaneous.rs
@@ -1,24 +1,26 @@
 pub trait ArgOrd<T: PartialOrd> {
-    fn arg_max(&self) -> Option<usize>;
-    fn arg_min(&self) -> Option<usize>;
+    fn arg_max(&self) -> usize;
+    fn arg_min(&self) -> usize;
 
     fn min_val(&self) -> T;
     fn max_val(&self) -> T;
 }
 
 impl<T: PartialOrd + Copy> ArgOrd<T> for Vec<T> {
-    fn arg_max(&self) -> Option<usize> {
+    fn arg_max(&self) -> usize {
         self.iter()
             .enumerate()
             .max_by(|(_, v1), (_, v2)| v1.partial_cmp(v2).unwrap())
             .map(|(idx, _)| idx)
+            .unwrap()
     }
 
-    fn arg_min(&self) -> Option<usize> {
+    fn arg_min(&self) -> usize {
         self.iter()
             .enumerate()
             .min_by(|(_, v1), (_, v2)| v2.partial_cmp(v1).unwrap())
             .map(|(idx, _)| idx)
+            .unwrap()
     }
 
     fn min_val(&self) -> T {
